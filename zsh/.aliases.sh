90c45a0f1c5d671e5a29fde142501851076d0f7b
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2022-03-25 13:57:32 +0000

    Aliases

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -1,10 +1,10 @@
 
 alias ap='readlink -e "$1"' # Absolute path of the argument
-alias t='(wezterm -d $PWD) &> /dev/null &'  
+alias t='(wezterm start --cwd $PWD) &> /dev/null &'
 
 # Find containers in all the cluster
 # $ c-ps <container-substring>
-alias c-ps='pssh -h ~/.aws-hosts.txt -i "docker ps -a | grep $1"'
+alias c-ps='pssh -h ~/.aws-hosts.txt -i "docker ps -a --format \"table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\" | sed \"s/758826220996.dkr.ecr.eu-west-1.amazonaws.com\///\" | grep $1"'
 
 # View logs of a container in an instance
 # $ i-log <instance-index> <container-substring> [<docker logs options>]
@@ -15,3 +15,8 @@ alias gitg='gitahead'
 yay-unused() {
     yay -Rns $(yay -Qtdq)
 }
+
+alias tpx='terraform plan -out x'
+alias tax='terraform apply x'
+
+alias flx='amm ~/meshapp/aws-tools/scripts/deploy.sc'
