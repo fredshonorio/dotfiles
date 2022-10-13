6efcac1857c70f4bb01e265a2a13b1287c3335ce
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2022-10-13 17:08:19 +0100

    Tabless

--- stowAll.sh
+++ stowAll.sh
@@ -6,20 +6,20 @@ first_char=$(hostname | cut -c1)
 
 # laptop stuff
 if [ "$first_char" = "l" ]; then
-	sudo stow -t / disable-caps-lock
-	stow xbindkeys-laptop
+    sudo stow -t / disable-caps-lock
+    stow xbindkeys-laptop
 fi
 
 
 stow \
-	git \
-	sakura \
-	xmonad \
-	xmobar \
-	wezterm \
-	terraform \
-	zsh \
-	user-bin \
+    git \
+    sakura \
+    xmonad \
+    xmobar \
+    wezterm \
+    terraform \
+    zsh \
+    user-bin \
     autostart
 
 sudo stow -t / google-drive
