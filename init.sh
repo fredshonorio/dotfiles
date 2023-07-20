253694bc0689e212247bc72ba45f5e647a85920f
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2023-07-20 16:42:17 +0100

    Replace docker override

--- init.sh
+++ init.sh
@@ -1,27 +1,19 @@
 #!/bin/sh
 
-#
-# This should be run only the first time nix and zsh is set up
-#
-
 set -ex
 
+#
+# This should be run only the first time zsh and docker are set up
+#
+
+
 sudo pacman -Syu
-
 sudo pacman --needed -S yay
+sudo pacman --needed -S chezmoi
 
-# docker
-sudo pacman --needed -S docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
-sudo mkdir /etc/systemd/system/docker.service.d
-# docker won't work unless you restart
-
-./packagesManjaro.sh
+chezmoi apply
 
 # Change shell 
 sudo chsh -s $(which zsh) $USER
-
-./update.sh
 
 echo "You should close this terminal and open a new one now."
