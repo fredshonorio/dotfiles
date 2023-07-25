b8921503a46cbb5c6df2c707db663646079864a8
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2023-07-25 11:03:07 +0100

    Add xbindkeys

--- stowAll.sh
+++ stowAll.sh
@@ -2,21 +2,6 @@
 
 set -ex
 
-first_char=$(hostname | cut -c1)
-
-# laptop stuff
-if [ "$first_char" = "l" ]
-then
-    stow xbindkeys-laptop
-
-    # prevent headphone buzz when no audio is playing
-    sudo stow -t / disable-audio-powersave
-    # systemctl start disable-audio-powersave
-    # systemctl enable disable-audio-powersave
-else
-    stow xbindkeys
-fi
-
 # delete installed config
 if [ ! -L "/etc/default/earlyoom" ]; then
     echo "deleting non-link file /etc/default/earlyoom"
