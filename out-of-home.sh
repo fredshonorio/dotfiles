fbb54332df88a073789c51024e126bc7508cf9f8
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2023-11-16 09:13:08 +0000

    IJ

--- out-of-home.sh
+++ out-of-home.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 # utilities
 
@@ -11,14 +11,31 @@ enable-systemd-service () {
 }
 
 replace-file-su () {
-    src="out-of-home-files/$1"
-    dst="$2"
+    local src="out-of-home-files/$1"
+    local dst="$2"
     sudo rsync $src $dst
 }
 
 yay-needed() {
     pkg="$1"
     yay -S $(yay -Qi $pkg 2>&1 >/dev/null | grep "error: package" | grep "was not found" | cut -d"'" -f2 | tr "\n" " ")
+}
+
+# create a file if it doesn't exist, fail if it differs
+ensure-file () {
+    local src="out-of-home-files/$1"
+    local dst="$2"
+
+    if [[ ! -f "$dst" ]]; then # destination doesn't exist, copy
+        sudo rsync $src $dst
+    else
+        if cmp --silent -- "$dst" "$src"; then
+            echo -e "\e[32m'$dst': exists and is identical\033[0m"
+        else
+            echo -e "\e[31m'$dst': exists but doesn't match\033[0m"
+            exit 1
+        fi
+    fi
 }
 
 #
@@ -43,3 +60,9 @@ replace-file-su "workfox" "/usr/local/bin/workfox"
 #
 # caps lock
 replace-file-su "disable-caps-lock.sh" "/etc/X11/xinit/xinitrc.d/99-disable-caps-lock.sh"
+
+#
+# intellij 
+# technically in $HOME, but not managed by home manager because idk how IJ
+# handles a file with no write permissions
+ensure-file xwin-copy.xml "$HOME/.config/JetBrains/IdeaIC2023.1/keymaps/XWin copy.xml"
