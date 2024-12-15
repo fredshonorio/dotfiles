c70960bf557863a28173cdda878fa9565b2f6a41
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2024-12-15 20:51:01 +0000

    Remove battery (use tray), use icons for dnd

--- files/polybar/dnd.sh
+++ files/polybar/dnd.sh
@@ -8,7 +8,7 @@ dnd_status=$(xfconf-query -c xfce4-notifyd -p /do-not-disturb)
 if [[ $dnd_status == true ]]
 then
     # colors.green in polybar/config.ini
-    echo '%{F#A6E22E}dnd!%{F-}'
+    echo ' 󰪑 '
 else
-    echo "dnd?"
+    echo " 󰂚 "
 fi
