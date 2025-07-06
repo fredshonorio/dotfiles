9e8c55276007814f905ea93e2117b6dafc64e333
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2025-07-06 08:56:46 +0100

    it's back

--- common.nix
+++ common.nix
@@ -105,7 +105,7 @@ in {
       (app "discord" "/usr/bin/discord")
       (app "obsidian" "/usr/bin/obsidian")
       (app "signal" "/usr/bin/signal-desktop")
-      (app "evolution" "/usr/bin/evolution")
+      (app "thunderbird" "/usr/bin/thunderbird")
       (app "xbindkeys" "/usr/bin/xbindkeys")
       (app "xmonad" "/usr/bin/xmonad --replace")
       (app "workrave" "/usr/bin/workrave")
