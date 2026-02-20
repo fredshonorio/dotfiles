62d3372e48f9ac90140d7651991ae2977cd6d22c
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2026-02-20 22:48:19 +0000

    try bar.wezterm

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -1,76 +1,38 @@
 local wezterm = require 'wezterm';
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez");
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 
-print(tabline.get_theme())
-tabline.setup({
-  options = {
-    --theme_overrides = {
-     -- tab = {
-       --       active = { fg = '#89b4fa', bg = '#313244' },
-        --inactive = { fg = '#cdd6f4', bg = '#181825' },
-      --}
-    --},
-    icons_enabled = true,
-    theme = 'Espresso',
-    tabs_enabled = true,
-    theme_overrides = {},
-    section_separators = {
-      left = wezterm.nerdfonts.pl_left_hard_divider,
-      right = wezterm.nerdfonts.pl_right_hard_divider,
-    },
-    component_separators = {
-      left = wezterm.nerdfonts.pl_left_soft_divider,
-      right = wezterm.nerdfonts.pl_right_soft_divider,
-    },
-    tab_separators = {
-      left = wezterm.nerdfonts.pl_left_hard_divider,
-      right = wezterm.nerdfonts.pl_right_hard_divider,
-    },
-  },
-  sections = {
-    tabline_a = { },
-    tabline_b = { },
-    tabline_c = { },
-    tab_active = {
-      
-{Attribute={Underline="Curly"}},
-      'index',
-      {Attribute={Underline="Curly"}},
-      { 'cwd', padding = { left = 0, right = 1 } },
-      {Attribute={Underline="Curly"}},
-      ":: ",
-      {Attribute={Underline="Curly"}},
-      { 'process'},
-    },
-    tab_inactive = { 'index', 
-      { 'cwd', padding = { left = 0, right = 1 } },
-      ":: ",
-      { 'process' },
-  },
-    tabline_x = { },
-    tabline_y = {  },
-    tabline_z = { 'hostname' },
-  },
-  extensions = {},
-})
-
-
-local config = {
-  font = wezterm.font("MesloLGS NF"),
-  font_size = 12.0,
-  exit_behavior = "Close",
-  check_for_updates = false,
-  enable_tab_bar = true,
-  color_scheme = "Espresso",
-  keys = {
-    {
-      key = 'Enter',
-      mods = 'SHIFT',
-      action = wezterm.action.SendString('\x1b[13;2u'),
-    },
-  }
+local opts = {
+    position = "top",
+    modules = {
+        workspace = {
+            enabled = false
+        },
+        pane = {
+            enabled = false
+        },
+        username = {
+            enabled = false
+        },
+        clock = {
+            enabled = false
+        }
+    }
 }
 
-tabline.apply_to_config(config)
+local config = {
+    font = wezterm.font("MesloLGS NF"),
+    font_size = 12.0,
+    exit_behavior = "Close",
+    check_for_updates = false,
+    enable_tab_bar = true,
+    color_scheme = "Espresso",
+    keys = {{
+        key = 'Enter',
+        mods = 'SHIFT',
+        action = wezterm.action.SendString('\x1b[13;2u')
+    }}
+}
+
+bar.apply_to_config(config, opts)
 
 return config;
