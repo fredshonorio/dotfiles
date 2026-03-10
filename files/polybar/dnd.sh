953b9723ecebf77c1ea99fa3fa1e6107f2bc5815
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2026-03-10 17:08:04 +0000

    fix dnd for dunst

--- files/polybar/dnd.sh
+++ files/polybar/dnd.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# inspired by
-# https://forum.manjaro.org/t/how-to-one-click-toggle-for-do-not-disturb-mode-for-xfce-notifications/143410
-
-dnd_status=$(xfconf-query -c xfce4-notifyd -p /do-not-disturb)
+dnd_status=$(dunstctl is-paused)
 
 if [[ $dnd_status == true ]]
 then
