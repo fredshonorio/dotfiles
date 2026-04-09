d377c047e9fe69a76e37222a05bbcfcad1afe338
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2026-04-09 09:06:29 +0100

    more cleaning up

--- init.sh
+++ init.sh
@@ -5,7 +5,9 @@ set -ex
 # this script should be idempotent
 
 # sudo pacman -Syu
-sudo pacman --needed -S yay
+if ! command -v yay > /dev/null 2>&1; then
+  sudo pacman -S yay
+fi
 
 ln -sfn ~/.dotfiles ~/.config/home-manager
 ln -sf "host-$(hostname).nix" ~/.dotfiles/home.nix # home.nix is gitignored, it's different for each host
@@ -19,6 +21,8 @@ if ! command -v home-manager > /dev/null 2>&1; then
 fi
 
 # Change shell 
-sudo chsh -s $(which zsh) $USER
+if [ "$SHELL" != "$(which zsh)" ]; then
+  chsh -s $(which zsh) $USER
+fi
 
 echo "You should close this terminal and open a new one now. Then run ./update.sh."
